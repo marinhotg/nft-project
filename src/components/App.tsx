829d9a9624ba2075cd63587c523650d2b318dfcb
Tiago Marinho <tiago.limarinho@gmail.com>
2023-04-26 14:15:36 -0300

    emoji de tela final

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import '../styles/App.css';
 import Logo from './Logo';
+import Emoji from './Emoji'
 
 function App() {
   const [isConnected, setIsConnected] = useState<boolean>(false);
@@ -19,10 +20,11 @@ function App() {
       <header className="App-header">
         <p>{isConnected ? (hasNFT ? "Parabéns, você já tem nossa NFT!" : "Você pode mintar nossa NFT!") : "Olá, você encontrou nossa NFT, conecte sua carteira!"}</p>
         <br/>
-        <Logo />
+        {!isConnected && <Logo />}
         <br/>
         {!isConnected && <button className='button' onClick={handleConnectClick}>Conectar</button>}
         {(isConnected && !hasNFT) && <button className='button' onClick={handleMintClick}>Pegar NFT!</button>}
+        {hasNFT && <Emoji />}
       </header>
     </div>
   );
---
+++ src/components/Emoji.tsx
@@ -0,0 +1,9 @@
+import '../styles/Emoji.css'
+
+const Emoji: React.FC = () => {
+    return (
+        <div><img className = "App-emoji" src='https://m.media-amazon.com/images/I/715vwvP5ZEL.png' alt='Emoji'/></div>
+    )
+}
+
+export default Emoji
